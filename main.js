7681dba6950553037b8aba270a3d3207a5fd6b72
njx100 <dnnguyen100@gmail.com>
2023-04-07 10:46:29 +0700

    finished all functions

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
 
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+
+function updateBody() {
+    if(inputEl.checked) {
+        bodyEl.style.background = "black";
+    } else {
+        bodyEl.style.background = "white";
+    }  
+};
+updateBody();
+
+function updateInput() {
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+};
+
+inputEl.addEventListener("input", () => {
+    updateBody();
+    updateInput();
+})
